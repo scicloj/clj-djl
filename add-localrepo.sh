7cfa9b9db9f71caa422fe6fd766885d1dca43815
kimim <kimi.im@outlook.com>
2020-09-15 14:17:29 +0800

    bad variable name for "-" seperated shell var

--- add-localrepo.sh
+++ add-localrepo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export clj-djl-version=0.1.1
+export version=0.1.1
 lein uberjar
 lein pom
-lein localrepo install target/clj-djl-$clj-djl-version.jar clj-djl $clj-djl-version
-mv pom.xml ~/.m2/repository/clj-djl/clj-djl/$clj-djl-version/clj-djl-$clj-djl-version.pom
+lein localrepo install target/clj-djl-$version.jar clj-djl $version
+mv pom.xml ~/.m2/repository/clj-djl/clj-djl/$version/clj-djl-$version.pom
