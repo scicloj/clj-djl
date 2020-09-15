932d0a476de06fab3b6a02d2459d2b519c2d20db
kimim <kimi.im@outlook.com>
2020-09-15 10:29:02 +0800

    update LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-djl "0.1.0-SNAPSHOT"
   :description "A clojure lib wraps deep java learning(DJL.ai)"
   :url "http://github.com/kimim/clj-djl"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  ;; https://mvnrepository.com/artifact/ai.djl.mxnet/mxnet-engine
                  [ai.djl/api "0.7.0"]
