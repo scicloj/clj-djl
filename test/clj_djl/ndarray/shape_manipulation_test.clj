d99fc6437d786fe66093abc72f9a6f2f2305b729
kimim <kimi.im@outlook.com>
2020-11-04 08:32:55 +0000

    add reshape-test

--- test/clj_djl/ndarray/shape_manipulation_test.clj
+++ test/clj_djl/ndarray/shape_manipulation_test.clj
@@ -65,6 +65,51 @@
              (nd/flatten array2)
              result)))))
 
+(deftest reshape-test
+  (with-open [ndm (nd/new-base-manager)]
+    (let [array (nd/create ndm (float-array [1. 2. 3. 4. 5. 6.]))
+          result (-> (nd/create ndm (float-array [1. 2. 3. 4. 5. 6.]))
+                     (nd/reshape [2 1 1 3]))]
+      (is (= (nd/reshape array [2 1 1 3]) result))
+      (is (= (nd/reshape array [-1 1 1 3]) result)))
+    (let [array (nd/create ndm [1. 2. 3. 4.])
+          result (nd/create ndm [1. 2. 3. 4.])]
+      (is (= (nd/flatten array) result)))
+    ;; multi-dim
+    (let [array (nd/create ndm (float-array [1. 2. 3. 4. 5. 6.]) (nd/shape [3 2]))
+          result (nd/create ndm (float-array [1. 2. 3. 4. 5. 6.]) (nd/shape [2 3]))]
+      (is (= (nd/reshape array [2 3]) result))
+      (is (= (nd/reshape array [2 -1]) result))
+      (is (= (nd/reshape array [-1 3]) result)))
+    (let [array (nd/create ndm [1. 2. 3. 4. 5. 6.] [3 2])
+          result (nd/create ndm [1. 2. 3. 4. 5. 6.] [2 3])]
+      (is (= (nd/reshape array [2 3]) result))
+      (is (= (nd/reshape array [2 -1]) result))
+      (is (= (nd/reshape array [-1 3]) result)))
+    ;; scalar
+    (let [array (nd/create ndm (float 5.))
+          result (nd/create ndm (float-array [5.]))]
+      (is (= (nd/reshape array [1]) result)))
+    (let [array (nd/create ndm 5.)
+          result (nd/create ndm [5.])]
+      (is (= (nd/reshape array [1]) result)))
+    ;; zero-dim
+    (let [array (nd/create ndm (nd/shape [1 0]))
+          result (nd/create ndm (nd/shape [2 3 0 1]))]
+      (is (= (nd/reshape array [2 3 0 1]) result)))
+    (let [array0 (nd/create ndm (nd/shape [2 0]))
+          array1 (nd/create ndm (nd/shape [3 0]))
+          array2 (nd/create ndm (nd/shape [0 3]))
+          result (nd/create ndm (nd/shape [2 3 0 1]))]
+      (is (= (nd/reshape array0 [2 3 0 1])
+             (nd/reshape array1 [2 3 0 1])
+             (nd/reshape array2 [2 3 0 1])
+             result)))))
+
+
+
+
+
 (comment
   (def ndm (nd/new-base-manager))
   (nd/flatten (nd/create ndm (nd/shape [0 2])))
