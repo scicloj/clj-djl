9969f044f481a716a1937548d5322107f4efe1f2
kimim <kimi.im@outlook.com>
2020-11-11 05:07:46 +0000

    update block test

--- test/clj_djl/training/block_test.clj
+++ test/clj_djl/training/block_test.clj
@@ -9,13 +9,13 @@
   (:import (ai.djl.training.initializer Initializer)))
 
 (deftest flatten-block
-  (def config (-> (loss/l2-loss) (train/new-default-training-config) (train/opt-initializer Initializer/ONES)))
-  (try-let [model (model/new-instance "model")]
-           (model/set-block model (nn/batch-flatten-block))
-           (try-let [trainer (model/new-trainer model config)
-                     manager (train/get-manager trainer)
-                     param-store (train/parameter-store manager false)
-                     data (nd/random-uniform manager 0 255 [10 28 28])
-                     expected (nd/reshape data [10 (* 28 28)])
-                     result (-> model model/get-block (nn/forward param-store (nd/ndlist data) true) nd/head)]
-                    (is (= result expected)))))
+  (let [config (-> (loss/l2-loss) (train/new-default-training-config) (train/opt-initializer Initializer/ONES))]
+    (with-open [model (-> (model/new-instance "model")
+                          (model/set-block (nn/batch-flatten-block)))
+                trainer (model/new-trainer model config)]
+      (let [manager (train/get-manager trainer)
+            param-store (train/parameter-store manager false)
+            data (nd/random-uniform manager 0 255 [10 28 28])
+            expected (nd/reshape data [10 (* 28 28)])
+            result (-> model model/get-block (nn/forward param-store (nd/ndlist data) true) nd/head)]
+        (is (= result expected))))))
