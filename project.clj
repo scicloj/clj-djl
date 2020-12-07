25eafb3531b4e9dd6cecd9bc20dad8cde23d4ebd
kimim <kimi.im@outlook.com>
2020-12-07 00:26:06 +0000

    plan to release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-djl/clj-djl "0.1.2"
+(defproject clj-djl/clj-djl "0.1.3"
   :description "A clojure lib wraps deep java learning(DJL.ai)"
   :url "http://github.com/kimim/clj-djl"
   :license {:name "Apache License, Version 2.0"
@@ -7,17 +7,16 @@
 
                  [org.slf4j/slf4j-simple "1.7.26"]
 
-                 [ai.djl/api "0.9.0-SNAPSHOT"]
-                 [ai.djl/model-zoo "0.9.0-SNAPSHOT"]
-                 [ai.djl/basicdataset "0.9.0-SNAPSHOT"]
+                 [ai.djl/api "0.8.0"]
+                 [ai.djl/model-zoo "0.8.0"]
+                 [ai.djl/basicdataset "0.8.0"]
 
-                 [ai.djl.mxnet/mxnet-engine "0.9.0-SNAPSHOT"]
+                 [ai.djl.mxnet/mxnet-engine "0.8.0"]
                  [ai.djl.mxnet/mxnet-native-auto "1.7.0-backport"]
 
                  [net.mikera/core.matrix "0.62.0"]
                  ;;[ai.djl.pytorch/pytorch-engine "0.8.0"]
                  ;;[ai.djl.pytorch/pytorch-native-auto "1.6.0"]
-                 [com.hypirion/clj-xchart "0.2.0"]
 
                  ;;[ai.djl.tensorflow/tensorflow-engine "0.8.0"]
                  ;;[ai.djl.tensorflow/tensorflow-native-auto "2.3.0"]
