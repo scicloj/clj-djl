e29352ed873accde8d7f5ec2dc5a8075b02b335b
kimim <kimi.im@outlook.com>
2021-03-12 14:48:41 +0800

    add more arithmetic test cases

--- test/clj_djl/ndarray/ndarray_element_arithmetic_test.clj
+++ test/clj_djl/ndarray/ndarray_element_arithmetic_test.clj
@@ -62,3 +62,46 @@
           add-all (ai.djl.ndarray.NDArrays/addi
                    to-add-all-array)]
       (is (= expected add-all (aget to-add-all-array 0))))))
+
+(deftest sub-scalar-test
+  (with-open [ndm (nd/new-base-manager)]
+    (let [minuend (nd/create ndm (float-array [6 9 12 11 0]))
+          result (ai.djl.ndarray.NDArrays/sub minuend 3)
+          in-place-result (ai.djl.ndarray.NDArrays/subi minuend 3)
+          expected (nd/create ndm (float-array [3 6 9 8 -3]))]
+      (is (= result expected in-place-result)))
+    (let [minuend (nd/create ndm (float-array [6 9 12 11 0]))
+          result (nd/- minuend 3)
+          in-place-result (nd/-! minuend 3)
+          expected (nd/create ndm (float-array [3 6 9 8 -3]))]
+      (is (= result expected in-place-result)))))
+
+(deftest sub-ndarray-test
+  (with-open [ndm (nd/new-base-manager)]
+    (let [minuend (nd/create ndm (float-array [6 9 12 15 0]))
+          subtrahend (nd/create ndm (float-array [2 3 4 5 6]))
+          result (ai.djl.ndarray.NDArrays/sub minuend subtrahend)
+          in-place-result (ai.djl.ndarray.NDArrays/subi minuend subtrahend)
+          expected (nd/create ndm (float-array [4 6 8 10 -6]))]
+      (is (= result expected in-place-result)))
+    (let [minuend (nd/create ndm (float-array [6 9 12 15 0]))
+          subtrahend (nd/create ndm (float-array [2 3 4 5 6]))
+          result (nd/- minuend subtrahend)
+          in-place-result (nd/-! minuend subtrahend)
+          expected (nd/create ndm (float-array [4 6 8 10 -6]))]
+      (is (= result expected in-place-result)))))
+
+
+(deftest reverse-sub-scalar-test
+  (with-open [ndm (nd/new-base-manager)]
+    (let [subtrahend (nd/create ndm (float-array [6 91 12 215 180]))
+          result (ai.djl.ndarray.NDArrays/sub 180 subtrahend)
+          in-place-result (ai.djl.ndarray.NDArrays/subi 180 subtrahend)
+          expected (nd/create ndm (float-array [174 89 168 -35 0]))]
+      (is (= result expected in-place-result)))
+    ;; let - accept both ndarray and scaler as the first parameter?
+    #_(let [subtrahend (nd/create ndm (float-array [6 91 12 215 180]))
+          result (nd/- 180 subtrahend)
+          in-place-result (nd/-! 180 subtrahend)
+          expected (nd/create ndm (float-array [174 89 168 -35 0]))]
+      (is (= result expected in-place-result)))))
